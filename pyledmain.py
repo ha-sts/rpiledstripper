f8b0690aaa40b61df236f1100470591b4ddc7425
dieneo <cfelris@gmail.com>
2013-12-29 10:21:45 -0700

    Main file for the python led strip script

---
+++ pyledmain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#Homeautomation cabinet lighting system
+
+#190 led pattern
+# - is true, there is an led present
+# x is false, there is no led present
+#Row A --------------------xxxxxxxxxxx
+
+#Row B ----------xxxxxxxxxx-------------
+#Logic: if a == true print. if a == false don't print. Same for row B.
+import RPi.GPIO as GPIO, time, patterns, ledSetup
+
+rowA = [190]
+rowB = [190]
+
+ledAPrint == 
+
+if rowA ==
+
+
